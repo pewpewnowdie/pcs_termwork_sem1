60367b6595edbf6f8f088610d1d9f8d59b69715d
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2022-12-16 18:20:52 +0530

    Update recursion_sum.c

--- recursion_sum.c
+++ recursion_sum.c
@@ -2,7 +2,8 @@
 Name:		Kshitij Tyagi
 Roll No.:	2021287
 Sec: 		F
-Problem Statement: Wrtie a program in C to calculate the sum of numbers from 1 to n using recursion.
+Problem Statement: 
+20. Wrtie a program in C to calculate the sum of numbers from 1 to n using recursion.
 */
 #include<stdio.h>
 int sum(int);
